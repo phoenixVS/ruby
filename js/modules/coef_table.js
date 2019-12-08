57bc6184f4430df7b9343f7ab5e8ac7b02ed59bf
Sergei <woronowserg@gmail.com>
2019-12-08 15:37:31 +0200

    coef_table added

--- js/modules/coef_table.js
+++ js/modules/coef_table.js
@@ -3,5 +3,23 @@ exports('coef_table', (params, done) => {
     ".coeficient-table": [
       "main/coeficient-table.html"
     ]
-  }, done);
-});
+  }, () => {
+    
+    $.ajax({
+      url: 'http://bestline.bet/inplay/',
+      success: function (data) {
+        
+        if (data != undefined) {
+          
+          $(`[data-id="coef-one"]`).text(data.DATA[0].CT[0].EV[0].MA[0].PA[0].OD.D);
+          $(`[data-id="coef-two"]`).text(data.DATA[0].CT[0].EV[0].MA[0].PA[2].OD.D);
+          $(`[data-id="coef-three"]`).text(data.DATA[0].CT[0].EV[0].MA[0].PA[1].OD.D);
+          
+        } else {
+          console.log("ERROR: Data is undefined")
+        }
+      }
+    });
+    done();
+  });
+});  
