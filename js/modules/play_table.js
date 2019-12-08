792b4f181f9ecd4bb5f24f9b4ec5d051cbf30051
Valentin Shapoval <valentin.vad.sh@gmail.com>
2019-12-08 15:12:40 +0200

    Play table editted

--- js/modules/play_table.js
+++ js/modules/play_table.js
@@ -13,48 +13,50 @@ exports('play_table', (params, done) => {
     })(0);
 
     const URL = "http://bestline.bet/inplay/";
-/*
-    function fillPlayTable(JSON) {
-      if(JSON != undefined) {
-        for (let i = 0; i < JSON.DATA.length; i++) {
-          for (let j = 0; j < 4; j++) {
-            if (JSON.DATA[i].CT[0].EV[j] != undefined) {
-              $(`[data-id="play-table"]`).append('<div class="row"> <div class="cell"> <div class="[ play-link ]"> <div class="[ play-link-block ]"> <p class="font m-white ellipsis">' + JSON.DATA[i].CT[0].EV[j].NA + '</p> <p class="font m-white ellipsis">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugiat porro consectetur ratione repellendus quae assumenda ducimus totam ipsam earum quas quos ex consequatur provident repellat voluptatibus eum? Aspernatur esse.</p> </div> <div class="[ play-link-block ] text-right"> <div class="sport-icon play"></div> <p class="font m-white">1 - 0</p> <p class="font m-white">87:03</p> </div> </div> </div> <div class="cell"> <button class="button coefficient">1/1</button> </div> <div class="cell"> <button class="button coefficient">1/1</button> </div> <div class="cell"> <button class="button coefficient">1/1</button> </div> </div>');            
-            } else {
-              break;
-            }
-          }
-        }
-        
-       //console.log(JSON.DATA[0].CT[0].EV[0].NA);
-      } else {
-        console.log("ERROR: Data is undefined");
-      }
-    }
-*/
 
-    function RenderTable(JSON ,ID) {
-      
+    function RenderTable(JSON, ID) {
+
       JSON.DATA.forEach(sport => {
-        if(parseInt(sport.ID) == ID) {
+        if (parseInt(sport.ID) == ID) {
 
           let mutchCounter = 0;
 
           for (let i = 0; i < sport.CT.length; i++) {
-            
+
             if (mutchCounter < 20) {
               if (sport.CT[i].EV.length < 4) {
                 for (let j = 0; j < sport.CT[i].EV.length; j++) {
-                  $(`[data-id="play-table"]`).append('<div class="row"> <div class="cell"> <div class="[ play-link ]"> <div class="[ play-link-block ]"> <p class="font m-white ellipsis">' + sport.CT[i].EV[j].NA + '</p> <p class="font m-white ellipsis">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugiat porro consectetur ratione repellendus quae assumenda ducimus totam ipsam earum quas quos ex consequatur provident repellat voluptatibus eum? Aspernatur esse.</p> </div> <div class="[ play-link-block ] text-right"> <div class="sport-icon play"></div> <p class="font m-white">' + sport.CT[i].EV[j].SS +'</p> <p class="font m-white">87:03</p> </div> </div> </div> <div class="cell"> <button class="button coefficient">1/1</button> </div> <div class="cell"> <button class="button coefficient">1/1</button> </div> <div class="cell"> <button class="button coefficient">1/1</button> </div> </div>');
+                  $(`[data-id="play-table"]`).append(`
+                  <div class="row">
+                   <div class="cell">
+                   <div class="[ play-link ]"> 
+                   <div class="[ play-link-block ]"> 
+                   <p class="font m-white ellipsis">${sport.CT[i].EV[j].NA.split('vs')[0]} vs</p>
+                   <p class="font m-white ellipsis">${sport.CT[i].EV[j].NA.split('vs')[1]}</p>
+                  </div> 
+                   <div class="[ play-link-block ] text-right"> <div class="sport-icon play"></div> <p class="font m-white">'${sport.CT[i].EV[j].SS}'</p> 
+                   <p class="font m-white">87:03</p> </div> </div> </div> 
+                   <div class="cell"> 
+                   <button class="button coefficient">1/1</button> </div> 
+                   <div class="cell"> 
+                   <button class="button coefficient">1/1</button> </div> 
+                   <div class="cell"> <button class="button coefficient">1/1</button> </div> </div>`);
                   mutchCounter++;
                 }
               } else {
                 for (let j = 0; j < 4; j++) {
-                  $(`[data-id="play-table"]`).append('<div class="row"> <div class="cell"> <div class="[ play-link ]"> <div class="[ play-link-block ]"> <p class="font m-white ellipsis">' + sport.CT[i].EV[j].NA + '</p> <p class="font m-white ellipsis">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugiat porro consectetur ratione repellendus quae assumenda ducimus totam ipsam earum quas quos ex consequatur provident repellat voluptatibus eum? Aspernatur esse.</p> </div> <div class="[ play-link-block ] text-right"> <div class="sport-icon play"></div> <p class="font m-white">' + sport.CT[i].EV[j].SS +'</p> <p class="font m-white">87:03</p> </div> </div> </div> <div class="cell"> <button class="button coefficient">1/1</button> </div> <div class="cell"> <button class="button coefficient">1/1</button> </div> <div class="cell"> <button class="button coefficient">1/1</button> </div> </div>');
+                  $(`[data-id="play-table"]`).append(`<div class="row"> <div class="cell"> <div class="[ play-link ]"> <div class="[ play-link-block ]"> <p class="font m-white ellipsis">'${sport.CT[i].EV[j].NA}'</p> <p class="font m-white ellipsis">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugiat porro consectetur ratione repellendus quae assumenda ducimus totam ipsam earum quas quos ex consequatur provident repellat voluptatibus eum? Aspernatur esse.</p> </div>
+                   <div class="[ play-link-block ] text-right"> <div class="sport-icon play"></div>
+                   <p class="font m-white">'${sport.CT[i].EV[j].SS}'</p>
+                   <p class="font m-white">87:03</p> </div> </div> </div> <div class="cell"> <button class="button coefficient">1/1</button> </div> 
+                  <div class="cell"> <button class="button coefficient">1/1</button> </div> <div class="cell"> <button class="button coefficient">1/1</button> </div> </div>`);
                   mutchCounter++;
                 }
               }
-              $(`[data-id="play-table"]`).append('<div class="row [ info ]"> <div class="cell"> <p class="font">' + sport.CT[i].NA + '</p> </div> <div class="cell"> <p class="font">1</p> </div> <div class="cell"> <p class="font">X</p> </div> <div class="cell"> <p class="font">2</p> </div></div>');
+              $(`[data-id="play-table"]`).append(`<div class="row [ info ]"> 
+              <div class="cell"> <p class="font">'${sport.CT[i].NA} '</p> </div> 
+              <div class="cell"> <p class="font">1</p> </div> 
+              <div class="cell"> <p class="font">X</p> </div> <div class="cell"> <p class="font">2</p> </div></div>`);
             } else {
               break;
             }
@@ -63,16 +65,16 @@ exports('play_table', (params, done) => {
           return true;
         }
       });
-      
+
     }
-    
+
     $.ajax({
       url: URL,
       success: function (data) {
-        
+
         if (data != undefined) {
           RenderTable(data, parseInt(data.DATA[0].ID));
-          
+
         } else {
           console.log("ERROR: Data is undefined")
         }
