633472087b04d04fa9d8773311007c4bd787920a
Sergei <woronowserg@gmail.com>
2019-12-08 16:51:38 +0200

    Routing fixed

--- js/modules/handlebar.js
+++ js/modules/handlebar.js
@@ -70,21 +70,6 @@ function emptyHandler() {
         });
 }
 
-
-    let json_data;
-
-    $.ajax({
-        url: 'http://bestline.bet/inplay/',
-        success: function (data) {
-      
-        if (data != undefined) {
-            json_data = data;        
-        } else {
-            console.log("ERROR: Data is undefined")
-        }
-        }
-    });
-
 // filters from slider
 function filterHandler(json, ID) {
 
@@ -209,11 +194,22 @@ function betslip_smallHandler() {
 
 function locationHashChanged() {
     let hash = window.location.hash.split('/');
-    switch (hash[1]) {
-        case 'filter': filterHandler(json_data, window.location.hash.substr(9)); break;
-        case 'game': gameHandler(); break;
-        case 'betslip': betslipHandler(); break;
-        case 'betslip-small': betslip_smallHandler(); break;
-        default: emptyHandler(); break;
-    }
+
+    $.ajax({
+        url: 'http://bestline.bet/inplay/',
+        success: function (data) {
+      
+        if (data != undefined) {
+            switch (hash[1]) {
+                case 'filter': filterHandler(data, window.location.hash.substr(9)); break;
+                case 'game': gameHandler(); break;
+                case 'betslip': betslipHandler(); break;
+                case 'betslip-small': betslip_smallHandler(); break;
+                default: emptyHandler(); break;
+            }        
+        } else {
+            console.log("ERROR: Data is undefined")
+        }
+        }
+    });
 }
