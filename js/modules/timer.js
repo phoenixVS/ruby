2f12233d0a6a217903fd72f0c803dd58fb1ee418
Sergei <woronowserg@gmail.com>
2019-12-09 19:40:42 +0200

    Added timer module

---
+++ js/modules/timer.js
@@ -0,0 +1,11 @@
+exports('timer', (params, done) => {
+    insertHtmlModules({}, () => {
+
+        function getTime(DC, TT, TU, TM, TS) {
+            
+        }
+        
+
+      done();
+    });
+  });
