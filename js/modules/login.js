280f1ce3a45509fed605d88300efcc9749c5b787
Sergei <woronowserg@gmail.com>
2020-01-15 20:18:51 +0200

    Login validation

--- js/modules/login.js
+++ js/modules/login.js
@@ -1,6 +1,61 @@
 exports('login', (params, done) => {
   insertHtmlModules({
   }, () => {
+
+    function sendLoginData(data) {
+      //TODO: sending data for validation
+      return true;
+    }   
+
+    function LoginHandler(login, blur, attempt_counter, data) {
+      let usr_name = $('#username').val();
+      let password = $('#password').val();
+
+      if (usr_name != '' && password != '') {
+
+        const loginForm = {
+          DATA: {
+            username: usr_name,
+            pass: password,
+          }
+        };
+
+        let accepted = sendLoginData(loginForm);
+
+        if (accepted) {
+          login.fadeOut('middle').remove("active");
+          blur.removeClass('block').addClass('none');
+          Cookies.set('logon', 'true');
+          console.log('success');
+        } else {
+          if (attempt_counter == 2) {
+            console.log(attempt_counter);
+            console.log('Blocking account...');
+          } else {
+            console.log('Unsuccess');
+            attempt_counter+=1;
+            $('.loginButton').on('click', () => {
+              LoginHandler(login, blur, attempt_counter);
+            });
+          }
+        }
+      } else {
+        if (attempt_counter == 2) {
+          console.log(attempt_counter);
+          console.log('Blocking account...');
+        } else {
+          console.log('Unsuccess');
+          attempt_counter+=1;
+          $('.loginButton').on('click', () => {
+            LoginHandler(login, blur, attempt_counter);
+          });
+        }
+      }
+    }
+
+    /*login.fadeOut('middle').remove("active");
+        blur.removeClass('block').addClass('none');
+        Cookies.set('logon', 'true');*/
     function renderLoginPopup() {
       let renderPromise = new Promise((resolve, reject) => {
         $('.blur').removeClass('none').addClass('block');
@@ -9,12 +64,12 @@ exports('login', (params, done) => {
             <div class="loginHeader">Login</div>
             <!--User Name-->
             <div class="inputLogin">
-              <input type="text" placeholder="User name" name="username" autocapitalize="off" autocomplete="off" autocorrect="off">
+              <input id="username" type="text" placeholder="User name" name="username" autocapitalize="off" autocomplete="off" autocorrect="off">
               <div class="clearButton"></div>
             </div>
             <!--Password-->
             <div class="inputPassword">
-              <input type="password" placeholder="Password" name="password" autocapitalize="off" autocomplete="off" autocorrect="off">
+              <input id="password" type="password" placeholder="Password" name="password" autocapitalize="off" autocomplete="off" autocorrect="off">
               <div class="showPassword"></div>
             </div>
             <div class="failedLogin">
@@ -45,12 +100,12 @@ exports('login', (params, done) => {
         // $(`[data-id=login]`).off('click', renderLoginPopup);
         const login = $('.loginContainer');
         const blur = $(`[data-id=blur]`);
+        var attempt_counter = 0;
+
         $('.loginButton').on('click', () => {
-          login.fadeOut('middle').remove("active");
-          blur.removeClass('block').addClass('none');
-          Cookies.set('logon', 'true');
-          //$(`[data-id=login]`).on('click', renderLoginPopup);
+          LoginHandler(login, blur, attempt_counter);
         });
+
         $("body").click(function (e) {
           if ($(e.target).closest(`[data-id=login]`).length != 0) return false; // disable trigger on first click to log in
           if ($(e.target).closest(`[data-id=loginContainer]`).length != 0) return false; // disable trigger on login popup
