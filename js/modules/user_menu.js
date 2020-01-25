da2d2d189c73b8c10b97397aa790b4da6f9c8d3b
root <root@DESKTOP-K9V46AA.localdomain>
2020-01-25 13:37:18 +0200

    user menu

--- js/modules/user_menu.js
+++ js/modules/user_menu.js
@@ -48,23 +48,40 @@ exports('user_menu', (params, done) => {
         $(`[data-id=login]`).off('click', renderUserMenu);
         const user_menu = $(`[data-id=user-menu]`);
         $('.loginButton').on('click', () => {
-          user_menu.slideUp().remove("active");
+          user_menu.slideUp("fast").remove("active");
           //$(`[data-id=login]`).on('click', renderUserMenu);
         });
         $("body").click(function (e) {
           if ($(e.target).closest(`[data-id=login]`).length != 0) return false; // disable trigger on first click to log in
           if ($(e.target).closest(`[data-id=user-menu]`).length != 0) return false; // disable trigger on login popup
-          user_menu.slideUp().remove("active").remove();
+          user_menu.slideUp("fast").remove("active").remove();
           //$(`[data-id=login]`).on('click', renderUserMenu);
         });
         // Account profile redirect
         let username = 'vasya1999';
-        $(`[data-id=accountHistory]`).prop('href', `#/user/${username}/transaction`);
-        $(`[data-id=accountSettings]`).prop('href', `#/user/${username}/balance/account`);
-        $(`[data-id=depositWithdraw]`).prop('href', `#/user/${username}/balance/deposit`);
-        $(`[data-id=promoBonuses]`).prop('href', `#`);
+        $(`[data-id=accountHistory]`).prop('href', `#/user/${username}/transaction`)
+          .on('click', () => {
+            window.location.hash = `/user/${username}/transaction`;
+            user_menu.slideUp("fast").remove("active").remove();
+          });
+        $(`[data-id=accountSettings]`).prop('href', `#/user/${username}/balance/account`)
+          .on('click', () => {
+            window.location.hash = `/user/${username}/balance/account`;
+            user_menu.slideUp("fast").remove("active").remove();
+          });
+        $(`[data-id=depositWithdraw]`).prop('href', `#/user/${username}/balance/deposit`)
+          .on('click', () => {
+            window.location.hash = `/user/${username}/balance/deposit`;
+            user_menu.slideUp("fast").remove("active").remove();
+          });
+        $(`[data-id=promoBonuses]`).prop('href', `#`)
+          .on('click', () => {
+            window.location.hash = ``;
+            user_menu.slideUp("fast").remove("active").remove();
+          });
         $(`[data-id=exitUser]`).on('click', (event) => {
-          user_menu.slideUp().remove("active").remove();
+          user_menu.slideUp("fast").remove("active").remove();
+          window.location.href = ``;
           Cookies.set('logon', 'false');
         });
       });
