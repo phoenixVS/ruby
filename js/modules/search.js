61d161dde4d06375832d334f5c4b9a1f09722680
Sergei <woronowserg@gmail.com>
2020-02-25 19:02:13 +0200

    search markets

--- js/modules/search.js
+++ js/modules/search.js
@@ -15,7 +15,7 @@ exports('search', (params, done) => {
       function RenderSearchResult(data) {
         new Promise( (resolve, reject) => {
         let choosen = false;
-        let choosen_NA = '';
+        let lastEV = '';
         let res_content = $('.search-result');
         res_content.empty();
         res_content.append(`
@@ -50,6 +50,7 @@ exports('search', (params, done) => {
               `);
               }
             } else if (data[i].type == 'EV') {
+                lastEV = data[i].NA;
                 res_content.append(`
                 <div class="search-ev">
                   <p class="font m-white">${data[i].NA}</p>
@@ -57,19 +58,67 @@ exports('search', (params, done) => {
                 `);
             } else if (data[i].type == 'MG') {
                 if ($(res_content.children(`.search-ev-links-${i}`)).length) {
-                  $(`.search-ev-links-${i}`).append(`
-                  <div class="s-ev-link">
-                    <p class="font white t-not-clicked">${data[i].NA}</p>
-                  </div>
-                  `);
-                } else {
-                  res_content.append(`
-                    <div class="search-ev-links-${0}">
-                      <div class="s-ev-link">
-                        <p class="font white t-not-clicked">${data[i].NA}</p>
+                  if (lastEV == 'Teams') {
+                    $(`.search-ev-links-${i}`).append(`
+                    <div class="s-ev-link">
+                      <p class="font white t-clicked">${data[i].NA}</p>
+                      <div class="t-market-group active">
+                        <div class="market-pa">
+                        <div class="market-pa-item">
+                            <span class="font m-white ellipsis" style="font-size: 15px;">Team vs Team</span>
+                            <span class="font m-white ellipsis" style="font-size: 12px;">Dd Mm Tt</span>
+                          </div>
+
+                          <div class="market-pa-item">
+                          <span class="font m-white ellipsis" style="font-size: 15px;">Team vs Team</span>
+                          <span class="font m-white ellipsis" style="font-size: 12px;">Dd Mm Tt</span>
+                          </div>
+                        </div>
+                        <div class="market-bets">
+                        </div>
                       </div>
                     </div>
                   `);
+                  } else {
+                    $(`.search-ev-links-${i}`).append(`
+                  <div class="s-ev-link">
+                    <p class="font white">${data[i].NA}</p>
+                  </div>
+                  `);
+                  }
+                } else {
+                  if (lastEV == 'Teams') {
+                    res_content.append(`
+                    <div class="search-ev-links-${0}">
+                      <div class="s-ev-link">
+                        <p class="font white t-clicked">${data[i].NA}</p>
+                        <div class="t-market-group active">
+                        <div class="market-pa">
+                          <div class="market-pa-item">
+                          <span class="font m-white ellipsis" style="font-size: 15px;">Team vs Team</span>
+                          <span class="font m-white ellipsis" style="font-size: 12px;">Dd Mm Tt</span>
+                          </div>
+
+                          <div class="market-pa-item">
+                          <span class="font m-white ellipsis" style="font-size: 15px;">Team vs Team</span>
+                          <span class="font m-white ellipsis" style="font-size: 12px;">Dd Mm Tt</span>
+                          </div>
+                        </div>
+                        <div class="market-bets">
+                        </div>
+                      </div>
+                      </div>
+                    </div>
+                  `);
+                  } else {
+                    res_content.append(`
+                    <div class="search-ev-links-${0}">
+                      <div class="s-ev-link">
+                        <p class="font white">${data[i].NA}</p>
+                      </div>
+                    </div>
+                  `);
+                  }
                 }
             }
         }
@@ -81,6 +130,10 @@ exports('search', (params, done) => {
             $(el.target).addClass('t-clicked');
             $(el.target).prop("onclick", null).off("click");
 
+            let marketDIV = $(el.target).parent().children('.t-market-group');
+            marketDIV.removeClass('not-active');
+            marketDIV.addClass('active');
+
             $(el.target).on('click', (item) => {
               eSetNotClicked(item);
             });
@@ -91,13 +144,17 @@ exports('search', (params, done) => {
             $(el.target).addClass('t-not-clicked');
             $(el.target).prop("onclick", null).off("click");
 
+            let marketDIV = $(el.target).parent().children('.t-market-group');
+            marketDIV.removeClass('active');
+            marketDIV.addClass('not-active');
+
             $(el.target).on('click', (item) => {
               eSetClicked(item);
             });
           }
 
-          $('.s-ev-link p.t-not-clicked').on('click', (el) => {
-            eSetClicked(el);
+          $('.s-ev-link p.t-clicked').on('click', (el) => {
+            eSetNotClicked(el);
           });
         });
       }
