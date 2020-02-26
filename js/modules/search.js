b01c9a58da9cb49e346309a10020e8fdd23d7e02
Sergei <woronowserg@gmail.com>
2020-02-26 18:23:39 +0200

    search sizing fix

--- js/modules/search.js
+++ js/modules/search.js
@@ -283,6 +283,7 @@ exports('search', (params, done) => {
       renderPromise.then(() => {
         // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
         let vh = window.innerHeight * 0.01;
+        console.log(vh);
         // Then we set the value in the --vh custom property to the root of the document
         document.querySelector('.main-search-container').style.setProperty('--vh', `${vh}px`);
         // We listen to the resize event
